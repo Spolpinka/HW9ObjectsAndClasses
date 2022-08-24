be47f6a79536dbdb97fdfabed329f451c3fb0a67
Spolpinka <a421243266@gmail.com>
2022-08-24 19:54:33 +0300

    hard level

---
+++ src/Book.java
@@ -0,0 +1,35 @@
+public class Book {
+    String nameOfBook; // имя книги
+    Author author; //автор книги
+    int yearOfPublication; //год публикации
+
+    public Book (String nameOfBook, Author author, int yearOfPublication){
+        this.nameOfBook = nameOfBook;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+//    public void setNameOfBook(String nameOfBook) {
+//        this.nameOfBook = nameOfBook;
+//    }
+
+//    public void setAuthor(Author author) {
+//        this.author = author;
+//    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public String getNameOfBook() {
+        return nameOfBook;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+
+
     public static void main(String[] args) {
+        Author author1 = new Author("Терри", "Пратчет");
+        Author author2 = new Author("Джоан","Роулинг");
+
+        Book harryPotter = new Book("Harry Potter", author2, 2000);
+        Book guardians = new Book("Guardians", author1, 1997);
+        // меняем год публикации
+        guardians.setYearOfPublication(1994);
+        // проверяем, изменился ли год публикации
+        System.out.println(guardians.yearOfPublication);
+
+        //создаем новый объект Library и задаем размер массива
+        Library lib = new Library(5);
+        //добавляем в новый объект lib (в новую библиотеку) класса Library ранее созданные книги
+        lib.addBook(harryPotter);
+        lib.addBook(guardians);
+        //печатаем библиотеку (проверяем метод печати всей lib
+        lib.toPrint();
+        // печатаем по названию книги (проверяем соответствующий метод)
+        lib.toPrintForNameOfBook(harryPotter.nameOfBook);
+        //меняем год публикации по названию и печатаем книгу для проверки работы соответствующего метода
+        lib.changeYearForNameOfBook(guardians.nameOfBook, 2000);
+        lib.toPrintForNameOfBook(guardians.nameOfBook);
 
     }
+
 }
